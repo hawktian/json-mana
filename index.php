dfde22b3471578ad73c448bc5dbfa519562350b4
Tonvin Tian <itonvin@gmail.com>
2023-03-14 09:37:54 +0800

    nocommit

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path)) as
 krsort($files);
 foreach ( $files ?? [] as $file) {
     echo '<p>';
-    echo '<a href="./view.php?name='.$file->getFilename().'" target=_blank>'.$file->getFilename().'</a>';
+    echo date('m-d H:i', $file->getCtime()).str_repeat('&nbsp;', 6).'<a href="./view.php?name='.$file->getFilename().'" target=_blank>'.$file->getFilename().'</a>';
     echo '&nbsp;&nbsp;&nbsp;';
     echo '<a href="./delete.php?name='.$file->getFilename().'" target=_self>删除</a>';
     echo '</p>';
